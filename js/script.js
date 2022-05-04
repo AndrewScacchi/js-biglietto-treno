e872b5644a496ef8f019ff417fbaa2d82439c3c2
andrea scacchi <andrea.scacchi@live.com>
2022-05-04 17:41:26 +0200

    Final commit, added "validation checks" to the input prompt

--- js/script.js
+++ js/script.js
@@ -13,8 +13,22 @@
 //prendiamo due dati numeri, controlliamo siano numero, forniamo avviso di errore in caso avverso
 
 let age = parseInt(prompt("Qual'è la tua età?")); 
-let distance = parseInt(prompt("Quanti kilometri devi percorrere?"));
+//let verify that age is a Num
+if (isNaN(age)){
+    alert("Questa età non è un valore accettato!");
+    age = parseInt(prompt("Inserire una età in formato YY"));
+} else {
+    alert("Età registrata. Grazie");
+}
 
+let distance = parseInt(prompt("Quanti kilometri devi percorrere?"));
+//let verify that distance is a Num
+if (isNaN(distance)){
+    alert("Questa distanza non è un valore accettato!");
+    distance = parseInt(prompt("Inserire una distanza da percorrere in kilometri"));
+} else {
+    alert("Distanza registrata. Grazie");
+}
 
 // calcoliamo uno sconto sulla base dell'età
 let discount;
@@ -29,13 +43,14 @@ if (age <= 17) {
 let priceTicket = (distance * 0.21);
 let priceDiscount = (priceTicket * discount);
 
+//Let print something on screen depending on age range
 if ( age <= 17){
-    document.writeln(`Il tuo ticket costa ${priceDiscount} euro da un prezzo originale di ${priceTicket} euro, con un 20% di sconto!`);
+    document.writeln(`Il tuo ticket costa ${priceDiscount} euro da un prezzo originale di ${priceTicket} euro, con un 20% di sconto! Perchè ti ta se un babbo!`);
 } else if( age >= 65){
-    document.writeln(`Il tuo ticket costa ${priceDiscount} euro da un prezzo originale di ${priceTicket} euro, con un 40% di sconto!`);
+    document.writeln(`Il tuo ticket costa ${priceDiscount} euro da un prezzo originale di ${priceTicket} euro, con un 40% di sconto! Perchè ti ta se vecio!`);
 } else {
-    document.writeln(`Il tuo ticket costa ${priceDiscount} euro da un prezzo originale di ${priceTicket} euro, con un 0% di sconto!`);
+    document.writeln(`Il tuo ticket costa ${priceDiscount} euro da un prezzo originale di ${priceTicket} euro, con uno 0% di sconto! Perchè fai schefe!`);
 }
-// document.writeln(`Il tuo ticket costa ${priceTicket} euri ` );
+
 
 
